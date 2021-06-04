4e01c06f4e03d53d2740e79c2951d4ace102c286
victor4pinheiro <vmcpinheiro@yahoo.com.br>
2021-06-04 15:12:39 -0300

    feat: add loop to read values from stdin and multiply all values from the same column

--- 24_matrix_product_of_column.c
+++ 24_matrix_product_of_column.c
@@ -11,7 +11,23 @@ int main()
         scanf("%d", &numberColumn);
 
 
-        int array[numberLine][numberColumn];
+        int matrix[numberLine][numberColumn];
+        int product[numberColumn];
+
+        for (int countLine = 0; countLine < numberLine; countLine++) {
+                for (int countColumn = 0; countColumn < numberColumn; countColumn++) {
+                        printf("Type the number of line %d and column %d:\n", countLine, countColumn);
+                        scanf("%d", &matrix[countLine][countColumn]);
+
+                        if (countLine == 0)
+                                product[countColumn] = 1;
+
+                        product[countColumn] *= matrix[countLine][countColumn];
+                }
+        }
+
+        for (int count = 0; count < numberColumn; count++)
+                printf("Product of column %d: %d\n", count, product[count]);
 
         return 0;
 }
